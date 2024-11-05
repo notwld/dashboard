9e90843937c2fd4ce0895b07806ba915141a6a33
notwld <mwfarrukh@gmail.com>
2024-11-05 09:13:03 -0500

    okok

--- frontend/src/app/dashboard/Leads/index.tsx
+++ frontend/src/app/dashboard/Leads/index.tsx
@@ -1,7 +1,4 @@
-import { SidebarTrigger } from '../../../components/ui/Sidebar/sidebar'
-import { Separator } from '../../../components/ui/Sidebar/separator'
-import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from '../../../components/ui/Sidebar/breadcrumb'
-import { Input } from '../../../components/ui/Sidebar/input'
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '../../../components/ui/Alert/alert'
 import * as React from "react"
 import {
     ColumnDef,
@@ -39,6 +36,11 @@ import {
 import { useNavigate } from 'react-router-dom'
 import { baseurl } from '../../../config/baseurl'
 import { Spinner } from '../../../components/ui/spinner'
+import { toast, useToast } from '../../../hooks/use-toaster'
+import { SidebarTrigger } from '../../../components/ui/Sidebar/sidebar'
+import { Separator } from '../../../components/ui/Sidebar/separator'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbList, BreadcrumbPage } from '../../../components/ui/Sidebar/breadcrumb'
+import { Input } from '../../../components/ui/Sidebar/input'
 
 
 export type Lead = {
@@ -59,126 +61,6 @@ export type Lead = {
 }
 
 
-export const columns: ColumnDef<Lead>[] = [
-    {
-        id: "select",
-        header: ({ table }) => (
-            <Checkbox
-                checked={
-                    table.getIsAllPageRowsSelected() ||
-                    (table.getIsSomePageRowsSelected() && "indeterminate")
-                }
-                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-                aria-label="Select all"
-            />
-        ),
-        cell: ({ row }) => (
-            <Checkbox
-                checked={row.getIsSelected()}
-                onCheckedChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    },
-
-
-    {
-        accessorKey: "date",
-        header: "Date",
-        cell: ({ row }) => <div>{row.getValue("date")}</div>,
-    },
-    {
-        accessorKey: "time",
-        header: "Time",
-        cell: ({ row }) => <div>{row.getValue("time")}</div>,
-    },
-    {
-        accessorKey: "platform",
-        header: "Platform",
-        cell: ({ row }) => <div>{row.getValue("platform")}</div>,
-    },
-    {
-        accessorKey: "assigned",
-        header: "Assigned",
-        cell: ({ row }) => <div>{row.getValue("assigned")}</div>,
-    },
-    {
-        accessorKey: "firstCall",
-        header: "First Call",
-        cell: ({ row }) => <div>{row.getValue("firstCall")}</div>,
-    },
-    {
-        accessorKey: "comments",
-        header: "Comments",
-        cell: ({ row }) => <div>{row.getValue("comments")}</div>,
-    },
-    {
-        accessorKey: "service",
-        header: "Service",
-        cell: ({ row }) => <div>{row.getValue("service")}</div>,
-    },
-    {
-        accessorKey: "name",
-        header: "Name",
-        cell: ({ row }) => <div>{row.getValue("name")}</div>,
-    },
-    {
-        accessorKey: "email",
-        header: "Email",
-        cell: ({ row }) => <div>{row.getValue("email")}</div>,
-    },
-    {
-        accessorKey: "number",
-        header: "Number",
-        cell: ({ row }) => <div>{row.getValue("number")}</div>,
-    },
-    {
-        accessorKey: "address",
-        header: "Address",
-        cell: ({ row }) => <div>{row.getValue("address")}</div>,
-    },
-    {
-        accessorKey: "credits",
-        header: "Credits",
-        cell: ({ row }) => <div>{row.getValue("credits")}</div>,
-    },
-    {
-        accessorKey: "cost",
-        header: "Cost",
-        cell: ({ row }) => <div>{row.getValue("cost")}</div>,
-    },
-    {
-        id: "actions",
-        enableHiding: false,
-        cell: ({ row }) => {
-            const lead = row.original
-
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => navigator.clipboard.writeText(lead.id)}
-                        >
-                            Copy Lead ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Edit</DropdownMenuItem>
-                        <DropdownMenuItem>Delete</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
-        },
-    },
-]
 
 export default function ManageLeads() {
     const [sorting, setSorting] = React.useState<SortingState>([])
@@ -193,6 +75,199 @@ export default function ManageLeads() {
     const [leads, setLeads] = React.useState([])
     const [loading, setLoading] = React.useState(false)
     const navigate = useNavigate()
+    const { toast } = useToast()
+    const [open, setOpen] = React.useState({
+        open: false,
+        id: ""
+    })
+    const columns: ColumnDef<Lead>[] = [
+        {
+            id: "select",
+            header: ({ table }) => (
+                <Checkbox
+                    checked={
+                        table.getIsAllPageRowsSelected() ||
+                        (table.getIsSomePageRowsSelected() && "indeterminate")
+                    }
+                    onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                    aria-label="Select all"
+                />
+            ),
+            cell: ({ row }) => (
+                <Checkbox
+                    checked={row.getIsSelected()}
+                    onCheckedChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false,
+        },
+
+
+        {
+            accessorKey: "date",
+            header: "Date",
+            cell: ({ row }) => {
+                const dateValue = new Date(row.getValue("date"));
+                const formattedDate = dateValue.toLocaleDateString("en-GB", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric"
+                });
+                return <div>{formattedDate}</div>;
+            },
+        },
+        {
+            accessorKey: "time",
+            header: "Time",
+            cell: ({ row }) => {
+                const timeString = row.getValue("time");
+                const [hours, minutes] = timeString.split(":").map(Number);
+                const date = new Date();
+                date.setHours(hours, minutes);
+
+                const formattedTime = date.toLocaleTimeString("en-US", {
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true
+                });
+
+                return <div>{formattedTime}</div>;
+            },
+        },
+        {
+            accessorKey: "platform",
+            header: "Platform",
+            cell: ({ row }) => <div>{row.getValue("platform")}</div>,
+        },
+        {
+            accessorKey: "assigned",
+            header: "Assigned",
+            cell: ({ row }) => <div>{row.getValue("assigned")}</div>,
+        },
+        {
+            accessorKey: "firstCall",
+            header: "First Call",
+            cell: ({ row }) => <div>{row.getValue("firstCall")}</div>,
+        },
+        {
+            accessorKey: "comments",
+            header: "Comments",
+            cell: ({ row }) => <div>{row.getValue("comments")}</div>,
+        },
+        {
+            accessorKey: "service",
+            header: "Service",
+            cell: ({ row }) => <div>{row.getValue("service")}</div>,
+        },
+        {
+            accessorKey: "name",
+            header: "Name",
+            cell: ({ row }) => <div>{row.getValue("name")}</div>,
+        },
+        {
+            accessorKey: "email",
+            header: "Email",
+            cell: ({ row }) => <div>{row.getValue("email")}</div>,
+        },
+        {
+            accessorKey: "number",
+            header: "Number",
+            cell: ({ row }) => <div>{row.getValue("number")}</div>,
+        },
+        {
+            accessorKey: "address",
+            header: "Address",
+            cell: ({ row }) => <div>{row.getValue("address")}</div>,
+        },
+        {
+            accessorKey: "credits",
+            header: "Credits",
+            cell: ({ row }) => <div>{row.getValue("credits")}</div>,
+        },
+        {
+            accessorKey: "cost",
+            header: "Cost",
+            cell: ({ row }) => <div>{row.getValue("cost")}</div>,
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                const lead = row.original
+
+                return (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                            <DropdownMenuItem
+                                onClick={() => navigator.clipboard.writeText(lead.id)}
+                            >
+                                Copy Lead ID
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Edit</DropdownMenuItem>
+                            <DropdownMenuItem onClick={() => {
+                                setOpen({
+                                    open: true,
+                                    id: lead.id
+                                })
+                            }}>
+                                Delete
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                );
+
+            },
+        },
+    ]
+    const deleteLead = async (id: string) => {
+        try {
+            const response = await fetch(baseurl + `/lead/delete-lead/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            const data = await response.json()
+            if (data.status) {
+                toast({
+                    title: "Lead Deletion Failed",
+                    description: `Lead could not be deleted. Reason: ${data.message}`,
+                    category: "error"
+                })
+                return
+            }
+            toast({
+                title: "Lead Deleted",
+                description: `Lead has been deleted.`,
+                category: "success"
+            })
+            fetchLeads()
+            setOpen({
+                open: false,
+                id: ""
+            })
+        } catch (error) {
+            console.error(error)
+            toast({
+                title: "Lead Deletion Failed",
+                description: `Lead could not be deleted.`,
+                category: "error"
+            })
+            setOpen({
+                open: false,
+                id: ""
+            })        }
+    }
     const table = useReactTable({
         data: leads,
         columns,
@@ -228,6 +303,29 @@ export default function ManageLeads() {
     }, [])
     return (
         <div className="w-full">
+            <AlertDialog open={open.open} onOpenChange={() => {
+                setOpen({
+                    open: false,
+                    id: ""
+                })
+            }}>
+
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle className="text-lg">Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription className="text-md">
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction onClick={() => deleteLead(open.id)}>
+                            Continue
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
             <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
                 <SidebarTrigger className="-ml-1" />
                 <Separator orientation="vertical" className="mr-2 h-4" />
